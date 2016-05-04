6ad05002337c2a5b5a452733648fbc9b9f8781d6
Giovanni Rescia <giovannirescia@gmail.com>
2016-05-04 18:32:43 -0300

    Ruedolph now jumps

---
+++ nivel-1/protagonista.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+import pilasengine
+from settings import IMG_DIR
+#pilas = pilasengine.iniciar(ancho=1600, alto=900)
+#f = pilas.fisica.Rectangulo(plataforma=1,x=-345,y=-85,alto=35,ancho=870,amortiguacion=0,restitucion=0)
+
+pilas = pilasengine.iniciar()
+
+class Ruedolph(pilasengine.actores.Actor):
+
+    def iniciar(self):
+        self.imagen = IMG_DIR + "/ruedolph_small.png"
+
+        self.figura = pilas.fisica.Circulo(self.x, self.y, 50,
+            friccion=0, restitucion=0,dinamica=1)
+
+        self.figura.sin_rotacion = True
+        self.figura.escala_de_gravedad = 3
+
+        self.sensor_pies = pilas.fisica.Rectangulo(self.x, self.y, 30, 5, sensor=True, dinamica=False,restitucion=0,amortiguacion=0)
+
+    def actualizar(self):
+        velocidad = 10
+        salto = 125
+        self.x = self.figura.x
+        self.y = self.figura.y
+        
+        if self.pilas.control.derecha:
+            self.figura.velocidad_x = velocidad
+            self.rotacion -= velocidad
+
+        elif self.pilas.control.izquierda:
+            self.figura.velocidad_x = -velocidad
+            self.rotacion += velocidad
+
+        else:
+            self.figura.velocidad_x = 0
+
+        if self.pilas.control.boton:
+            self.decir('putooooo')
+
+        if self.esta_pisando_el_suelo():
+            if self.pilas.control.arriba and not int(self.figura.velocidad_y):
+                self.figura.impulsar(0, salto)
+
+        self.sensor_pies.x = self.x
+        self.sensor_pies.y = self.y - 53
+
+    def esta_pisando_el_suelo(self):
+        return len(self.sensor_pies.figuras_en_contacto) > 0
+
+
+#pilas.actores.vincular(Ruedolph)
+#r = Protagonista(pilas)
+#f = pilas.fondos.Fondo(imagen=IMG_DIR+'/nivel-1/N1E1.png')
+#f.escala = 0.46
+#pilas.ejecutar()
--- nivel-1/settings.py
+++ nivel-1/settings.py
@@ -1,1 +1,9 @@
-IMG_DIR = '/home/giovanni/proyectos/outsider/imagenes'
+from os.path import dirname, abspath, join
+
+PROJECT_PATH = abspath(dirname(__file__))
+
+# SCRAPED_NEWS_PATH = join(PROJECT_PATH, './newsscraper/scraped-data/cnnnews.jsonlines')
+
+# LABELED_NEWS_PATH = join(PROJECT_PATH, './corpus/labeled-data/cnn-labeled-2703.xlsx')
+
+IMG_DIR = join(PROJECT_PATH, './imagenes')
