1f0811d90a62ec29fa94a5dd5f9c0471db6ab17e
Giovanni Rescia <giovannirescia@gmail.com>
2016-05-12 02:21:22 -0300

    ruedolph se maneja mas que tarzan ahora

--- nivel-1/protagonista.py
+++ nivel-1/protagonista.py
@@ -1,26 +1,11 @@
 # coding: utf-8
 import pilasengine
-import Box2D as box2d
-from pilasengine.fisica.figura import Figura
-from pilasengine import utils
-from pilasengine import fisica
 from settings import IMG_DIR
-#pilas = pilasengine.iniciar(ancho=1600, alto=900)
-#f = pilas.fisica.Rectangulo(plataforma=1,x=-345,y=-85,alto=35,ancho=870,amortiguacion=0,restitucion=0)
 
 # Variable global
 en_colision = False
 
 pilas = pilasengine.iniciar()
-
-class MC(object):
-    def __init__(self):
-        self.es_eje = True
-        self.eje = pilas.fisica.Circulo(y=-200,sensor=1, dinamica=0)
-# Ruedolph_Circle
-class RC(pilasengine.fisica.circulo.Circulo):
-    def __init__(self, fisica=fisica, pilas=pilas, x=0, y=0, radio=20, dinamica=True, densidad=1.0, restitucion=0.56, friccion=10.5, amortiguacion=0.1, sin_rotacion=False, sensor=False):
-        super(RC, self).__init__(fisica, pilas, x, y, radio, dinamica, densidad, restitucion, friccion, amortiguacion, sin_rotacion,sensor)
 
 class Ruedolph(pilasengine.actores.Actor):
 
@@ -32,7 +17,6 @@ class Ruedolph(pilasengine.actores.Actor):
         self.imantado = False
         self.figura.sin_rotacion = True
         self.figura.escala_de_gravedad = 3
-
         self.sensor_pies = pilas.fisica.Rectangulo(self.x, self.y, 30, 5, sensor=True, dinamica=False,restitucion=0,amortiguacion=0)
 
     def actualizar(self):
@@ -58,7 +42,6 @@ class Ruedolph(pilasengine.actores.Actor):
             self.figura.velocidad_x = 0
             self.figura.velocidad_y = 0
 
-
         if self.esta_pisando_el_suelo():
             if self.pilas.control.arriba and not int(self.figura.velocidad_y) and not pilas.control.boton:
                 self.figura.impulsar(0, salto)
@@ -82,21 +65,28 @@ class Pendorcho(pilasengine.actores.Actor):
 pilas.actores.vincular(Pendorcho)
 pilas.actores.vincular(Ruedolph)
 r = Ruedolph(pilas)
-t1 = Pendorcho(pilas, 0, -128)
-t2 = Pendorcho(pilas, 150, -90)
-#f = pilas.fondos.Fondo(imagen=IMG_DIR+'/nivel-1/N1E1.png')
-#f.escala = 0.46
+t1 = Pendorcho(pilas, 0, -100)
+t2 = Pendorcho(pilas, 180, -5)
+t3 = Pendorcho(pilas, -12, 85)
+t4 = Pendorcho(pilas, 165, 188)
 
 pendorchos = pilas.actores.Grupo()
 pendorchos.agregar(t1)
 pendorchos.agregar(t2)
-
+pendorchos.agregar(t3)
+pendorchos.agregar(t4)
 def verificar(evento):
     global en_colision
     if en_colision and pilas.control.boton:
         en_colision = False
-        r.figura.x = Pendorcho.get_x()
-        r.figura.y = Pendorcho.get_y()
+        aux = None
+        xs = r.figura.figuras_en_contacto
+        for elem in xs:
+            if isinstance(elem, pilasengine.fisica.circulo.Circulo):
+                if r.figura != elem:
+                    aux = elem
+        r.figura.x = aux.x
+        r.figura.y = aux.y
         pilas.fisica.gravedad_y = 0
         r.figura.velocidad_y = 0
         
@@ -104,11 +94,7 @@ def encajar(r, pendorchos):
     global en_colision
     en_colision = True
 
-
 pilas.eventos.pulsa_tecla.conectar(verificar)
 pilas.colisiones.agregar(r, pendorchos, encajar)
-
-
-
 
 pilas.ejecutar()
